07b009b8f45b319623a09090404d8956b1d98f86
Mohamed <mohamedelshazly18@yahoo.com>
2021-05-13 07:29:08 +0200

    Moved the project to React instead of html/css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Land from './Land';
+// import Land from './Land';
 import Redirect from './Redirect'
 import Home from './Home';
 import Bands from './Bands';
@@ -12,7 +12,7 @@ function App() {
       <div className="App">
         <Switch>
           {/* <Route exact path='/'><Land /></Route> */}
-          <Route path='/'><Home /></Route>
+          <Route exact path='/'><Home /></Route>
           <Route path='/bands'><Bands /></Route>
           <Route path='/band-details'><Details /></Route>
           <Route path='/redirect'><Redirect /></Route>
